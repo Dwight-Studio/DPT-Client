4c423220d1fd319c264505ba0dd6cf8926685cd6
Deleranax <9z3KI8ininGZmy>
2020-03-21 14:07:26 +0100

    Adding Logger Archives Files saved in logs/

--- dpt/main.py
+++ dpt/main.py
@@ -1,31 +1,48 @@
+import datetime
 import logging
 import argparse
 import sys
+import tarfile
+
 import pygame
+import os.path
 from pygame.locals import *
 
-VERSION = "BETA-0.0.1"
+VERSION = "ALPHA-0.0.1"
 PYTHON_VERSION = str(sys.version_info[0]) + "." + str(sys.version_info[1]) + "." + str(sys.version_info[2]) + "-" + str(sys.version_info[3])
 PYGAME_VERSION = pygame.version.ver
 PLATFORM = sys.platform
+ROOT_DIRECTORY = os.path.abspath("../")
 
-# Loggers
-LOGGING_FORMAT = logging.Formatter(fmt="[%(asctime)s][%(levelname)s][%(name)s] %(message)s", datefmt="%H:%M:%S")
+# Logs
+## Loggers
 main_logger = None
 
+## Formatter
+LOGGING_FORMAT = logging.Formatter(fmt="[%(asctime)s][%(levelname)s][%(name)s] %(message)s", datefmt="%H:%M:%S")
+
+## File handler
+if os.path.isfile(ROOT_DIRECTORY + "/logs/latest.log"):
+    file = tarfile.open(ROOT_DIRECTORY + "/logs/" + datetime.datetime.today().strftime("%d-%m-%Y-%H-%M-%S") + ".tar.gz", mode="x:gz", )
+    file.add(ROOT_DIRECTORY + "/logs/latest.log", arcname="latest.log")
+    file.close()
+    os.remove(ROOT_DIRECTORY + "/logs/latest.log")
+
+file_handler = logging.FileHandler(ROOT_DIRECTORY + "/logs/latest.log")
+file_handler.setFormatter(LOGGING_FORMAT)
+
+## Stream handler
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setFormatter(LOGGING_FORMAT)
+
+# Args
 parser = argparse.ArgumentParser(description='Start the game.')
 parser.add_argument('--debug', type=bool, help='enable/disable debug log', default=False)
 
 
-def get_main_logging(debug):
+def get_loggers(debug):
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
-
-    file_handler = logging.FileHandler("../latest.log")
-    file_handler.setFormatter(LOGGING_FORMAT)
-
-    stream_handler = logging.StreamHandler(sys.stdout)
-    stream_handler.setFormatter(LOGGING_FORMAT)
 
     if debug:
         file_handler.setLevel(logging.DEBUG)
@@ -46,7 +63,7 @@ def main():
     pygame.init()
     args = parser.parse_args()
 
-    main_logger = get_main_logging(True)
+    main_logger = get_loggers(args.debug)
 
     main_logger.info("--- Starting Don't Play Together. ---")
     main_logger.debug("Version: " + VERSION)
