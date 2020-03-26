a1d32755a899d9577dda74452cc85a38f16bd177
joshuadwightred <42832831+joshuadwightred@users.noreply.github.com>
2020-03-26 21:37:11 +0100

    Minor Update Prevent @WhiteRed from exploading

--- dpt/game.py
+++ dpt/game.py
@@ -69,7 +69,7 @@ class Game(object):
         self.clock = pygame.time.Clock()
 
         try:
-            # /!\ ZONE SECURISEE /!\
+            # /!\ ZONE SECURISÉE /!\
             from dpt.engine.loader import RessourceLoader
             self.ressources = RessourceLoader()
             self.ressources.add_pending("*")
