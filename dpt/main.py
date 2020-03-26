3d56d4c7a27d78b9a5497c58286d65025620c2f0
Deleranax <9z3KI8ininGZmy>
2020-03-26 17:57:04 +0100

    Minor update Adding primitive launcher

--- dpt/main.py
+++ dpt/main.py
@@ -1,10 +1,14 @@
 import argparse
+import os
+import sys
 
 parser = argparse.ArgumentParser(description='Start the game.')
 parser.add_argument('--debug', help='enable debug logging', action="store_true")
 
 
 def main():
+    print("MAIN: Adding " + os.path.pardir + " to PYTHONPATH")
+    sys.path.insert(0, os.path.pardir)
     from dpt.game import Game
     global game
     args = parser.parse_args()
---
+++ launch.bat
@@ -0,0 +1,5 @@
+@echo off
+cd dpt
+python main.py
+cd ..
+echo on
