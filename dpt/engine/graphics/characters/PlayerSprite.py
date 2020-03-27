518c7f998bec72c22286bfbf4e3afbdff712f9e3
joshuadwightred <42832831+joshuadwightred@users.noreply.github.com>
2020-03-27 12:32:43 +0100

    Major update - Sprites are now working (well not really but for now it's great)

--- dpt/engine/graphics/characters/PlayerSprite.py
+++ dpt/engine/graphics/characters/PlayerSprite.py
@@ -11,7 +11,7 @@ class PlayerSprite(pygame.sprite.Sprite):
     walkRight = game.ressources.get_multiple("dpt.images.characters.player.R*")
     walkLeft = game.ressources.get_multiple("dpt.images.characters.player.L*")
 
-    def __init__(self, color, width, height, x, y):
+    def __init__(self, color, x, y, width, height):
         pygame.sprite.Sprite.__init__(self)  # Sprite's constructor called
         self.image = pygame.Surface((width, height))
         self.image.fill(color)
