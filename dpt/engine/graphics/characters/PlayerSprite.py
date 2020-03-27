f5df83c662fc7451bea6e9b3e40d80826338b3fd
joshuadwightred <42832831+joshuadwightred@users.noreply.github.com>
2020-03-27 13:03:41 +0100

    Major update - We are now in an only sprites mood. - Removing Player.py and its subsequent class.

--- dpt/engine/graphics/characters/PlayerSprite.py
+++ dpt/engine/graphics/characters/PlayerSprite.py
@@ -13,8 +13,7 @@ class PlayerSprite(pygame.sprite.Sprite):
 
     def __init__(self, color, x, y, width, height):
         pygame.sprite.Sprite.__init__(self)  # Sprite's constructor called
-        self.image = pygame.Surface((width, height))
-        self.image.fill(color)
+        self.image = PlayerSprite.char
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
--- dpt/engine/mainLoop.py
+++ dpt/engine/mainLoop.py
@@ -1,8 +1,6 @@
 import pygame
-import math
 
 from dpt.game import Game
-from dpt.engine.graphics.characters.Player import Player
 from dpt.engine.graphics.characters.PlayerSprite import PlayerSprite
 
 
@@ -10,7 +8,6 @@ def redraw_game_window():
     game = Game.get_instance()
     game.sprites.update()
     game.surface.blit(bg, (0, 0))
-    game.player.draw(game.surface)
     game.sprites.draw(game.surface)
     game.window.update()
 
@@ -22,7 +19,6 @@ def loop():
     screen_width, screen_height = game.surface.get_size()
     game.playerSprite = PlayerSprite((255, 0, 0), 300, screen_height - 200, 64, 64)
     game.sprites.add(game.playerSprite)
-    game.player = Player(300, screen_height - 100, 64, 64)
     bg = game.ressources.get("dpt.images.environment.background")
     run = True
     while run:
@@ -31,52 +27,6 @@ def loop():
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 run = False
-
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_LEFT] and game.player.x > game.player.vel:
-            game.player.x -= game.player.vel
-            game.player.left = True
-            game.player.right = False
-            game.player.standing = False
-        elif keys[pygame.K_LEFT]:
-            game.player.x = 0
-            game.player.left = True
-            game.player.right = False
-            game.player.standing = False
-        elif keys[pygame.K_RIGHT] and game.player.x + game.player.width + game.player.vel < screen_width:
-            game.player.x += game.player.vel
-            game.player.left = False
-            game.player.right = True
-            game.player.standing = False
-        elif keys[pygame.K_RIGHT]:
-            game.player.x = screen_width - game.player.width
-            game.player.left = False
-            game.player.right = True
-            game.player.standing = False
-        else:
-            game.player.standing = True
-            game.player.walkCount = 0
-
-        if not game.player.isJump:
-            if keys[pygame.K_UP]:
-                game.player.isJump = True
-                game.player.left = False
-                if game.player.right:
-                    game.player.right = True
-                else:
-                    game.player.right = False
-                game.player.walkCount = 0
-        else:
-            if game.player.jumpCount >= -game.player.CONSTJUMPCOUNT:
-                neg = 1
-                if game.player.jumpCount < 0:
-                    neg = -1
-                game.player.y -= math.floor((game.player.jumpCount ** 2) * 0.5) * neg
-                game.player.jumpCount -= 1
-            else:
-                game.player.isJump = False
-                game.player.jumpCount = game.player.CONSTJUMPCOUNT
 
         redraw_game_window()
 
