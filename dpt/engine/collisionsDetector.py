3f4f4c64011ec14693c48843d31a113d72f811e1
joshuadwightred <42832831+joshuadwightred@users.noreply.github.com>
2020-04-03 11:50:27 +0200

    Major update - Sprites are finally working - Removing Player.py and its subsequent class. - Adding hitboxes that are bugged - Platforms' creation

--- dpt/engine/collisionsDetector.py
+++ dpt/engine/collisionsDetector.py
@@ -3,7 +3,7 @@ import pygame
 from dpt.game import Game
 
 
-class Detector(object):
+class Detector:
     def __init__(self):
         game = Game.get_instance()
         hits = pygame.sprite.spritecollide(game.playerSprite, game.platforms, False)
@@ -11,3 +11,9 @@ class Detector(object):
             for platformes in hits:
                 print("Point en haut à gauche :", platformes.rect.x, platformes.rect.y)
                 print("Point en bas à droite :", platformes.rect.x + platformes.width, platformes.rect.y + platformes.height)
+                if platformes.rect.x - game.playerSprite.rect.x <= 0:
+                    if game.playerSprite.isFalling:
+                        game.playerSprite.onPlatform = True
+                        game.playerSprite.isJump = False
+                        game.playerSprite.jumpCount = game.playerSprite.CONSTJUMPCOUNT
+                        game.playerSprite.isFalling = False
