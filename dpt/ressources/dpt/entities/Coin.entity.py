a51592964f95091df108e6886c0ab06b4d43fe4d
Deleranax <reazer18@gmail.com>
2020-04-11 18:02:22 +0200

    Pathing bugs - Crash when /logs does not exist

--- dpt/ressources/dpt/entities/Coin.entity.py
+++ dpt/ressources/dpt/entities/Coin.entity.py
@@ -6,7 +6,7 @@ from dpt.engine.loader import RessourceLoader
 
 
 class Coin(pygame.sprite.Sprite):
-    texture = "dpt.images.environment.coins.1"
+    texture = "dpt.images.environment.coins.00"
     textures = "dpt.images.environment.coins.*"
     screen_width, screen_height = Game.surface.get_size()
 
@@ -31,12 +31,12 @@ class Coin(pygame.sprite.Sprite):
         self.image.set_alpha(self.alpha)
 
     def animation(self):
-        if self.animCount + 1 >= 40:
+        if self.animCount + 1 >= 72:
             self.animCount = 0
         else:
             self.animCount += 1
 
-        self.image = self.frames[math.floor(self.animCount/10)]
+        self.image = self.frames[self.animCount//2]
         self.image = pygame.transform.scale(self.image, (self.width, self.height))
 
     def collide(self):
