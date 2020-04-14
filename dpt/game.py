ab8edadb42e5a6e932d4b3037577278e7f4de0a5
Deleranax <reazer18@gmail.com>
2020-04-14 18:12:43 +0200

    Adding Builds

--- dpt/game.py
+++ dpt/game.py
@@ -18,7 +18,7 @@ def get_root():
 
 class Game(object):
     # Constantes
-    VERSION = "ALPHA-0.3.4"
+    VERSION = "ALPHA-0.1.0"
     PYTHON_VERSION = str(sys.version_info[0]) + "." + str(sys.version_info[1]) + "." + str(sys.version_info[2]) + "-" + str(sys.version_info[3])
     PYGAME_VERSION = pygame.version.ver
     PLATFORM = sys.platform
