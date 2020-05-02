13deedb7f9e81d5b71f2b7ef402724b071d03f9f
joshuadwightred <42832831+joshuadwightred@users.noreply.github.com>
2020-05-02 20:25:56 +0200

    Major update - Enemies now have the new collision engine.

--- dpt/ressources/dpt/entities/EnemySprite.entity.py
+++ dpt/ressources/dpt/entities/EnemySprite.entity.py
@@ -2,7 +2,9 @@ import math
 
 import pygame
 
+from dpt.engine.gui.editor.tileEditor import TileEditor
 from dpt.engine.loader import RessourceLoader
+from dpt.engine.tileManager import TileManager
 from dpt.game import Game
 
 
@@ -13,6 +15,7 @@ class EnemySprite(pygame.sprite.Sprite):
     height = math.floor(90 * Game.DISPLAY_RATIO)
     offset_x = (Game.TILESIZE - width) // 2
     offset_y = 0
+    mask = "dpt.images.characters.player.mask"
 
     def __init__(self, x, y):
         from dpt.engine.tileManager import TileManager
@@ -24,6 +27,8 @@ class EnemySprite(pygame.sprite.Sprite):
         self.right = True
         self.standing = False
         self.image = pygame.transform.smoothscale(self.image, (self.width, self.height))
+        self.mask = pygame.mask.from_surface(pygame.transform.scale(RessourceLoader.get(EnemySprite.mask),
+                                                                    (self.width, self.height)))
         self.CONSTHEIGT = self.height
         self.CONSTWIDTH = self.width
         self.rect = self.image.get_rect()
@@ -47,113 +52,99 @@ class EnemySprite(pygame.sprite.Sprite):
         self.maxvelocity = 2
         self.lowGravity = False
         self.gravityModifier = 0
-        self.monsterimmortal = True
+        self.monsterimmortal = False
         self.big = False
 
     def update(self):
-        from dpt.engine.tileManager import TileManager
-        if not Game.freeze_game:
+        if not TileEditor.in_editor:
+            from dpt.engine.tileManager import TileManager
+            if not Game.freeze_game:
 
-            if self.Slow:
-                self.maxvelocity = 1
-            else:
-                self.maxvelocity = 2
+                Game.add_debug_info("Enemy.left = " + str(self.left))
+                Game.add_debug_info("Enemy.right = " + str(self.right))
 
-            if self.lowGravity:
-                self.gravityModifier = 0.02
-            else:
-                self.gravityModifier = 0
+                if self.Slow:
+                    self.maxvelocity = 1
+                else:
+                    self.maxvelocity = 2
 
-            if self.monsterimmortal and not self.big:
-                self.height = math.floor(self.height * 1.4)
-                self.width = math.floor(self.width * 1.4)
-                self.big = True
-                self.rect[2] //= 0.71
-                self.rect[3] //= 0.71
-                self.image = pygame.transform.smoothscale(self.image, (self.width, self.height))
-            elif not self.monsterimmortal:
-                self.height = self.CONSTHEIGT
-                self.width = self.CONSTWIDTH
-                self.big = False
-                self.rect[2] = self.CONSTRECT2
-                self.rect[3] = self.CONSTRECT3
-                self.image = pygame.transform.smoothscale(self.image, (self.width, self.height))
+                if self.lowGravity:
+                    self.gravityModifier = 0.02
+                else:
+                    self.gravityModifier = 0
 
-            if self.left:
-                if self.xvel > 0 and not self.Ice:
+                if self.monsterimmortal and not self.big:
+                    self.height = math.floor(self.height * 1.4)
+                    self.width = math.floor(self.width * 1.4)
+                    self.big = True
+                    self.rect[2] //= 0.71
+                    self.rect[3] //= 0.71
+                    self.image = pygame.transform.smoothscale(self.image, (self.width, self.height))
+                elif not self.monsterimmortal:
+                    self.height = self.CONSTHEIGT
+                    self.width = self.CONSTWIDTH
+                    self.big = False
+                    self.rect[2] = self.CONSTRECT2
+                    self.rect[3] = self.CONSTRECT3
+                    self.image = pygame.transform.smoothscale(self.image, (self.width, self.height))
+
+                if self.left:
+                    if self.xvel > 0 and not self.Ice:
+                        self.xvel = 0
+                    if self.xvel > -self.maxvelocity * Game.DISPLAY_RATIO:
+                        self.xvel -= (self.maxvelocity / 2) * Game.DISPLAY_RATIO
+                    self.left = True
+                    self.right = False
+                    self.standing = False
+                elif self.right:
+                    if self.xvel < 0 and not self.Ice:
+                        self.xvel = 0
+                    if self.xvel < self.maxvelocity * Game.DISPLAY_RATIO:
+                        self.xvel += (self.maxvelocity / 2) * Game.DISPLAY_RATIO
+                    self.left = False
+                    self.right = True
+                    self.standing = False
+                else:
                     self.xvel = 0
-                if self.xvel > -self.maxvelocity * Game.DISPLAY_RATIO:
-                    self.xvel -= (self.maxvelocity / 2) * Game.DISPLAY_RATIO
-                self.left = True
-                self.right = False
-                self.standing = False
-            elif self.right:
-                if self.xvel < 0 and not self.Ice:
-                    self.xvel = 0
-                if self.xvel < self.maxvelocity * Game.DISPLAY_RATIO:
-                    self.xvel += (self.maxvelocity / 2) * Game.DISPLAY_RATIO
-                self.left = False
-                self.right = True
-                self.standing = False
-            else:
-                self.xvel = 0
-                self.standing = True
+                    self.standing = True
 
-            self.lastx = self.rect.left
-            self.rect.left += self.xvel
-            self.collide(self.xvel, 0, TileManager.environment_group)
+                self.lastx = self.rect.x
 
-            if not (self.isJump and not self.Slow) or (self.Slow and self.frameCount % 3 == 0):
-                self.allowJump = False
-                self.gravityCount += 1
-                self.gravity = math.floor((self.gravityCount ** 2) * (0.05 - self.gravityModifier) * Game.DISPLAY_RATIO) * -1
-                self.rect.top -= self.gravity
-                self.collide(0, self.gravity, TileManager.environment_group)
-                self.frameCount += 1
-            else:
-                self.frameCount += 1
-        self.animation()
+                if not (self.isJump and not self.Slow) or (self.Slow and self.frameCount % 3 == 0):
+                    self.allowJump = False
+                    self.gravityCount += 1
+                    self.gravity = math.floor((self.gravityCount ** 2) * (0.05 - self.gravityModifier) * Game.DISPLAY_RATIO) * -1
+                    self.yvel = self.gravity
+                    self.frameCount += 1
+                else:
+                    self.frameCount += 1
 
-        if self.lastx == self.rect.left:
-            self.left = not self.left
-            self.right = not self.right
+            self.securityTime -= 1
+            if self.check_fall(TileManager.environment_group):
+                self.left = not self.left
+                self.right = not self.right
 
-        self.securityTime -= 1
-        if self.check_fall(TileManager.environment_group):
-            self.left = not self.left
-            self.right = not self.right
+                if self.left:
+                    self.xvel -= 1 * Game.DISPLAY_RATIO
+                elif self.right:
+                    self.xvel += 1 * Game.DISPLAY_RATIO
 
-            if self.left:
-                self.xvel -= 1 * Game.DISPLAY_RATIO
-            elif self.right:
-                self.xvel += 1 * Game.DISPLAY_RATIO
+            self.maskcollide()
 
-        self.rect.top -= self.yvel
-        self.collide(0, self.yvel, TileManager.environment_group)
-        self.check_void()
+            self.rect.left += math.floor(self.xvel)
+            self.rect.top -= math.floor(self.yvel)
+
+            self.animation()
+
+            if self.lastx == self.rect.x:
+                self.left = not self.left
+                self.right = not self.right
+
+            self.check_void()
 
     def animation(self):
         # pygame.draw.rect(Game.surface, (255, 0, 0), self.rect, 2)
         pass
-
-    def collide(self, x_vel_delta, y_vel_delta, platforms):
-        for i in platforms:
-            if i.rect.colliderect(Game.display_rect):
-                if pygame.sprite.collide_rect(self, i):
-                    if x_vel_delta > 0:
-                        self.rect.right = i.rect.left
-                        self.xvel = 0
-                    if x_vel_delta < 0:
-                        self.rect.left = i.rect.right
-                        self.xvel = 0
-                    if y_vel_delta < 0:
-                        self.rect.bottom = i.rect.top
-                        self.onPlatform = True
-                        self.jumpCount = self.CONSTJUMPCOUNT
-                        self.allowJump = True
-                        self.gravityCount = 0
-                    if y_vel_delta > 0:
-                        self.rect.top = i.rect.bottom
 
     def check_void(self):
         if self.rect.top > 2000:
@@ -165,14 +156,77 @@ class EnemySprite(pygame.sprite.Sprite):
                 neg = 1
             else:
                 neg = -1
-            self.rect.left += self.width * Game.DISPLAY_RATIO * neg
-            self.rect.top += self.height * Game.DISPLAY_RATIO
+            self.rect.x += self.width * Game.DISPLAY_RATIO * neg
+            self.rect.y += self.height * Game.DISPLAY_RATIO
             for i in platforms:
                 if pygame.sprite.collide_rect(self, i):
-                    self.rect.left -= self.width * Game.DISPLAY_RATIO * neg
-                    self.rect.top -= self.height * Game.DISPLAY_RATIO
+                    self.rect.x -= self.width * Game.DISPLAY_RATIO * neg
+                    self.rect.y -= self.height * Game.DISPLAY_RATIO
                     return False
-            self.rect.left -= self.width * Game.DISPLAY_RATIO * neg
-            self.rect.top -= self.height * Game.DISPLAY_RATIO
+            self.rect.x -= self.width * Game.DISPLAY_RATIO * neg
+            self.rect.y -= self.height * Game.DISPLAY_RATIO
             self.securityTime = 60
             return True
+
+    def maskcollide(self):
+        for i in TileManager.environment_group:
+            if i.rect.colliderect(Game.display_rect):
+                rx = i.rect.x - (self.rect.x + math.floor(self.xvel))
+                ry = i.rect.y - (self.rect.y - math.floor(self.yvel))
+
+                if self.mask.overlap(i.mask, (rx, ry)):
+                    dx = 0
+                    dy = 0
+
+                    if math.floor(self.yvel) == 0:
+                        mask = self.mask.overlap_mask(i.mask, (rx, ry))
+                        b_rects = mask.get_bounding_rects()
+                        for rect in b_rects:
+                            if -8 * Game.DISPLAY_RATIO <= rect.height <= 8 * Game.DISPLAY_RATIO:
+                                dy = rect.height
+                                self.yvel = 0
+                                self.onPlatform = True
+                                self.gravityCount = 0
+                                self.isJump = False
+                                self.allowJump = True
+                                self.jumpCount = self.CONSTJUMPCOUNT
+                                self.frameCount = 0
+                            break
+
+                    crx = i.rect.x - self.rect.x
+                    mask = self.mask.overlap_mask(i.mask, (crx, ry))
+                    b_rects = mask.get_bounding_rects()
+                    for rect in b_rects:
+                        if self.rect.y < i.rect.y:
+                            dy = rect.height + math.floor(self.yvel)
+                            self.yvel = 0
+                            self.onPlatform = True
+                            self.gravityCount = 0
+                            self.isJump = False
+                            self.allowJump = True
+                            self.jumpCount = self.CONSTJUMPCOUNT
+                            self.frameCount = 0
+                        elif self.rect.y > i.rect.y:
+                            dy = - rect.height + math.floor(self.yvel)
+                            self.yvel = 0
+                            self.isJump = False
+                            self.allowJump = False
+                            self.jumpCount = self.CONSTJUMPCOUNT
+                        break
+
+                    self.rect.y -= dy
+
+                    cry = (i.rect.y - self.rect.y)
+                    mask = self.mask.overlap_mask(i.mask, (rx, cry))
+                    b_rects = mask.get_bounding_rects()
+                    for rect in b_rects:
+                        Game.add_debug_info("dx = " + str(dx))
+                        if self.rect.x > i.rect.x:
+                            dx = rect.width + math.floor(self.xvel)
+                            self.xvel = 0
+                        elif self.rect.x < i.rect.x:
+                            dx = - rect.width + math.floor(self.xvel)
+                            self.xvel = 0
+                        break
+
+                    self.rect.x += dx
