95186c70951805729b91296424986ad16e2b5695
Deleranax <reazer18@gmail.com>
2020-05-15 18:23:55 +0200

    Some patches

--- dpt/engine/fileManager.py
+++ dpt/engine/fileManager.py
@@ -2,6 +2,8 @@ import json
 import os
 import tkinter as tk
 from tkinter import filedialog
+
+from dpt.engine.scenes import Scenes
 from dpt.engine.tileManager import TileManager
 from dpt.engine.loader import RessourceLoader
 from dpt.game import Game
@@ -15,25 +17,28 @@ class FileManager:
     @classmethod
     def import_file(cls):
         """Permet de charger un fichier .level.json"""
-        root = tk.Tk()
-        root.withdraw()
-        rfile = filedialog.askopenfilename(parent=root, title="Sélectionner un niveau", filetypes=[("Fichier de niveau DPT", "*.level.json"), ("Tous les fichiers", "*")], initialdir=cls.defaultDir)
         try:
-            with open(rfile) as f:
-                data = json.load(f)
-                wfile = str(cls.defaultDir) + "/" + str(os.path.basename(rfile))
-                with open(wfile, "w") as fw:
-                    data2 = json.dumps(data, indent=4)
-                    fw.write(data2)
-                RessourceLoader.RESSOURCES["user.levels." + str(os.path.basename(rfile)).split(".")[0]] = wfile
-                RessourceLoader.add_pending("user.levels." + str(os.path.basename(rfile)).split(".")[0])
-                RessourceLoader.load()
-                TileManager.load_level("user.levels." + str(os.path.basename(rfile)).split(".")[0])
-                cls.log.info("Successfully loaded : " + str(rfile))
+            root = tk.Tk()
+            root.withdraw()
+            rfile = filedialog.askopenfilename(parent=root, title="Sélectionner un niveau", filetypes=[("Fichier de niveau DPT", "*.level.json"), ("Tous les fichiers", "*")], initialdir=cls.defaultDir)
+            try:
+                with open(rfile) as f:
+                    data = json.load(f)
+                    wfile = str(cls.defaultDir) + "/" + str(os.path.basename(rfile))
+                    with open(wfile, "w") as fw:
+                        data2 = json.dumps(data, indent=4)
+                        fw.write(data2)
+                    RessourceLoader.RESSOURCES["user.levels." + str(os.path.basename(rfile)).split(".")[0]] = wfile
+                    RessourceLoader.add_pending("user.levels." + str(os.path.basename(rfile)).split(".")[0])
+                    RessourceLoader.load()
+                    TileManager.load_level("user.levels." + str(os.path.basename(rfile)).split(".")[0])
+                    cls.log.info("Successfully loaded : " + str(rfile))
+                    root.destroy()
+            except UnreachableRessourceError and FileNotFoundError:
+                cls.log.warning("Unable to load file : " + str(rfile))
                 root.destroy()
-        except UnreachableRessourceError and FileNotFoundError:
-            cls.log.warning("Unable to load file : " + str(rfile))
-            root.destroy()
+        except json.decoder.JSONDecodeError as ex:
+            Scenes.return_error("Impossible de charger le niveau", "Détails :", "json.decoder.JSONDecodeError: " + str(ex))
 
     @classmethod
     def save_file(cls, level):
