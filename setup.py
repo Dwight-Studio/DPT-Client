ff6c3a9c9b7d56652e79dd6e13770b95d5704ab9
Deleranax <reazer18@gmail.com>
2020-05-19 17:45:55 +0200

    Patching build

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from cx_Freeze import setup, Executable
 path = sys.path + [os.path.abspath(".")]
 
 # options d'inclusion/exclusion des modules
-includes = ["pygame", "json", "requests", "tkinter", "psutil", "math"]  # nommer les modules non trouves par cx_freeze
+includes = ["pygame", "json", "requests", "tkinter", "psutil", "math", "tkinter.simpledialog", "tkinter.filedialog"]  # nommer les modules non trouves par cx_freeze
 excludes = ["dpt"]
 packages = []  # nommer les packages utilises
 
