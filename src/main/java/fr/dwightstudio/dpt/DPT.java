3c459a2e0176277bbba50d9a707f0087f99bc1ee
Deleranax <deleranax@gmail.com>
2021-05-30 01:04:06 +0200

    ResourceManager Enhancement
    Adding a generified and customisable ResourceManager to allow the game dev to add proprietary file type compatibility.

--- src/main/java/fr/dwightstudio/dpt/DPT.java
+++ src/main/java/fr/dwightstudio/dpt/DPT.java
@@ -2,9 +2,7 @@ package fr.dwightstudio.dpt;
 
 import fr.dwightstudio.dpt.engine.graphics.GLFWWindow;
 import fr.dwightstudio.dpt.engine.logging.GameLogger;
-import fr.dwightstudio.dpt.engine.resources.ResourceManager;
 
-import static fr.dwightstudio.dpt.engine.Engine.ENGINE_FULLSCREEN;
 import static fr.dwightstudio.dpt.engine.Engine.ENGINE_WINDOWED;
 
 public class DPT {
