2e65949ca0159bc4b58bf8ab8446f086fe775cb2
Deleranax <deleranax@gmail.com>
2021-05-30 16:26:31 +0200

    Enhancing loggers
    Adding a better format (I copied Minecraft and @GamerMine disagree) and renaming the main thread.

--- src/main/java/fr/dwightstudio/dpt/DPT.java
+++ src/main/java/fr/dwightstudio/dpt/DPT.java
@@ -10,6 +10,7 @@ public class DPT {
     public GLFWWindow window = new GLFWWindow(1280, 720, ENGINE_WINDOWED);
 
     public void run(){
+        Thread.currentThread().setName("Main Render Thread");
         GameLogger.init();
         window.init();
     }
